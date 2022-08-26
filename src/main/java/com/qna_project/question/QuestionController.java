56a0ca3cb1818c17803755901e22d592cc6d4386
gimminjae <min356812@naver.com>
2022-08-26 22:30:15 +0900

    create : QuestionController생성 및 /question/list url 매핑

---
+++ src/main/java/com/qna_project/question/QuestionController.java
@@ -0,0 +1,19 @@
+package com.qna_project.question;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/question")
+@RequiredArgsConstructor
+public class QuestionController {
+
+    @GetMapping("/list")
+    @ResponseBody
+    public String list() {
+        return "question_list";
+    }
+}
