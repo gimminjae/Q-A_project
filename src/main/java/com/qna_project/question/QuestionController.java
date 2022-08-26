21b76cd662e9e34567899ac6c5302215c3d7321b
gimminjae <min356812@naver.com>
2022-08-26 22:33:53 +0900

    create : question_list.html생성 및 매핑

--- src/main/java/com/qna_project/question/QuestionController.java
+++ src/main/java/com/qna_project/question/QuestionController.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class QuestionController {
 
     @GetMapping("/list")
-    @ResponseBody
     public String list() {
         return "question_list";
     }
