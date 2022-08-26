71d263e17f6c904abf108b479e17590032bafc8a
gimminjae <min356812@naver.com>
2022-08-26 22:38:56 +0900

    질문 리스트 보여주기 : /question/list

--- src/main/java/com/qna_project/question/QuestionController.java
+++ src/main/java/com/qna_project/question/QuestionController.java
@@ -2,17 +2,23 @@ package com.qna_project.question;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/question")
 @RequiredArgsConstructor
 public class QuestionController {
+    private final QuestionService questionService;
 
     @GetMapping("/list")
-    public String list() {
+    public String list(Model model) {
+        List<Question> questionList = questionService.getList();
+        model.addAttribute("questionList", questionList);
         return "question_list";
     }
 }
