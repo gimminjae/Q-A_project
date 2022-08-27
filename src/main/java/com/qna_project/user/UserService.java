23e3ee37282652eb49757d2d4042a2551664c034
gimminjae <min356812@naver.com>
2022-08-27 11:45:56 +0900

    PasswordEncoder bean 등록해 사용

--- src/main/java/com/qna_project/user/UserService.java
+++ src/main/java/com/qna_project/user/UserService.java
@@ -2,19 +2,20 @@ package com.qna_project.user;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     public void create(String username, String password, String email) {
         SiteUser siteUser = new SiteUser();
         siteUser.setUsername(username);
         siteUser.setEmail(email);
 
-        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
         siteUser.setPassword(passwordEncoder.encode(password));
         userRepository.save(siteUser);
     }
