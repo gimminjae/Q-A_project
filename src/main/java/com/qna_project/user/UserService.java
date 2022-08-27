a33d8cec22d80dc38c20f75cd355b4996d10283e
gimminjae <min356812@naver.com>
2022-08-27 11:44:01 +0900

    userService에 create 구현

--- src/main/java/com/qna_project/user/UserService.java
+++ src/main/java/com/qna_project/user/UserService.java
@@ -1,6 +1,7 @@
 package com.qna_project.user;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -8,6 +9,13 @@ import org.springframework.stereotype.Service;
 public class UserService {
     private final UserRepository userRepository;
 
-    public void create(String username, String password1, String email) {
+    public void create(String username, String password, String email) {
+        SiteUser siteUser = new SiteUser();
+        siteUser.setUsername(username);
+        siteUser.setEmail(email);
+
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        siteUser.setPassword(passwordEncoder.encode(password));
+        userRepository.save(siteUser);
     }
 }
